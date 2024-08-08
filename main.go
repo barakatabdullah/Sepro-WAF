907a1852ab03a325e5bb2a532d0bf8d8297bc0bc
Barakat <abdallah771363885@gmail.com>
2024-08-09 00:22:34 +0300

    feat : config coraza waf on the revers proxy

--- main.go
+++ main.go
@@ -1,25 +1,62 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+
+	"github.com/corazawaf/coraza/v3"
+	txhttp "github.com/corazawaf/coraza/v3/http"
+	"github.com/corazawaf/coraza/v3/types"
 )
 
-func main() {
+func initProxy() *httputil.ReverseProxy {
 	// Create a reverse proxy
-	targetURL, err := url.Parse("http://localhost:5173") // Replace with your target URL
+	targetURL, err := url.Parse("http://localhost:4000") // Replace with your target URL
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	proxy := httputil.NewSingleHostReverseProxy(targetURL)
 
+	return proxy
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	proxy := initProxy()
+	proxy.ServeHTTP(w, r)
+}
+
+func createWAF() coraza.WAF {
+	directivesFile := "./default.conf"
+	if s := os.Getenv("DIRECTIVES_FILE"); s != "" {
+		directivesFile = s
+	}
+
+	waf, err := coraza.NewWAF(
+		coraza.NewWAFConfig().
+			WithErrorCallback(logError).
+			WithDirectivesFromFile(directivesFile),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return waf
+}
+
+func logError(error types.MatchedRule) {
+	msg := error.ErrorLog()
+	fmt.Printf("[logError][%s] %s\n", error.Rule().Severity(), msg)
+}
+
+func main() {
+
+	waf := createWAF()
+
 	// Start the server
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		proxy.ServeHTTP(w, r)
-	})
+	http.Handle("/", txhttp.WrapHandler(waf, http.HandlerFunc(handler)))
 
 	log.Println("Reverse proxy server started on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
